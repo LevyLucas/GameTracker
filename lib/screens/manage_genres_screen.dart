77c856ae8e2c0b047483de3426087dccc78e1a8e
Lucas Levy <lucaslevy@id.uff.br>
2024-07-11 17:50:07 -0300

    genre disable in offline mode

--- lib/screens/manage_genres_screen.dart
+++ lib/screens/manage_genres_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/user_provider.dart';
 import '../database_helper.dart';
 
 class ManageGenresScreen extends StatefulWidget {
@@ -18,6 +20,8 @@ class _ManageGenresScreenState extends State<ManageGenresScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final userId = Provider.of<UserProvider>(context).userId;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Manage Genres'),
@@ -26,35 +30,44 @@ class _ManageGenresScreenState extends State<ManageGenresScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Form(
-              key: _formKey,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      controller: _genreController,
-                      decoration: InputDecoration(labelText: 'Genre Name'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter a genre name';
+            if (userId != null)
+              Form(
+                key: _formKey,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _genreController,
+                        decoration: InputDecoration(labelText: 'Genre Name'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a genre name';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await DatabaseHelper().addGenre(_genreController.text);
+                          _genreController.clear();
+                          setState(() {});
                         }
-                        return null;
                       },
                     ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        await DatabaseHelper().addGenre(_genreController.text);
-                        _genreController.clear();
-                        setState(() {});
-                      }
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+            if (userId == null)
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Não pode adicionar generos em modo offline.',
+                  style: TextStyle(fontSize: 16, color: Colors.red),
+                ),
+              ),
             Expanded(
               child: FutureBuilder<List<Map<String, dynamic>>>(
                 future: DatabaseHelper().getGenres(),
