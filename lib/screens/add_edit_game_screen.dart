ddb583782a8e63fc99d6136570476c703011ef31
Lucas Levy <lucaslevy@id.uff.br>
2024-07-11 18:01:13 -0300

    add genres on add edit game

--- lib/screens/add_edit_game_screen.dart
+++ lib/screens/add_edit_game_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/game_provider.dart';
 import '../providers/user_provider.dart';
+import '../database_helper.dart';
 
 class AddEditGameScreen extends StatefulWidget {
   final Map<String, dynamic>? game;
@@ -17,6 +18,8 @@ class _AddEditGameScreenState extends State<AddEditGameScreen> {
   late String _name;
   late String _description;
   late String _releaseDate;
+  int? _selectedGenre;
+  List<Map<String, dynamic>> _genres = [];
 
   @override
   void initState() {
@@ -25,7 +28,17 @@ class _AddEditGameScreenState extends State<AddEditGameScreen> {
       _name = widget.game!['name'];
       _description = widget.game!['description'];
       _releaseDate = widget.game!['release_date'];
+      _selectedGenre = widget.game!['genre_id'];
     }
+    _loadGenres();
+  }
+
+  Future<void> _loadGenres() async {
+    final dbHelper = DatabaseHelper();
+    final genres = await dbHelper.getGenres();
+    setState(() {
+      _genres = genres;
+    });
   }
 
   @override
@@ -74,6 +87,27 @@ class _AddEditGameScreenState extends State<AddEditGameScreen> {
                 },
                 onSaved: (value) => _releaseDate = value!,
               ),
+              DropdownButtonFormField<int>(
+                value: _selectedGenre,
+                decoration: InputDecoration(labelText: 'Genre'),
+                items: _genres.map((genre) {
+                  return DropdownMenuItem<int>(
+                    value: genre['id'],
+                    child: Text(genre['name']),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedGenre = value;
+                  });
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please select a genre';
+                  }
+                  return null;
+                },
+              ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
@@ -85,12 +119,14 @@ class _AddEditGameScreenState extends State<AddEditGameScreen> {
                         'description': _description,
                         'release_date': _releaseDate,
                         'user_id': userId,
+                        'genre_id': _selectedGenre,
                       });
                     } else {
                       Provider.of<GameProvider>(context, listen: false).updateGame(widget.game!['id'], {
                         'name': _name,
                         'description': _description,
                         'release_date': _releaseDate,
+                        'genre_id': _selectedGenre,
                       });
                     }
                     Navigator.pop(context);
